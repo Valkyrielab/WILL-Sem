15cd22256c400c3f35de6ee48ee2356b236fb801
Valkyrielab <michelle@hotdesk.co.za>
2025-11-12 14:52:10 +0200

    v

--- part 1/script.js
+++
@@ -1,7 +0,0 @@
-// Dropdown navigation script
-        document.getElementById('action-select').addEventListener('change', function() {
-            const selectedPage = this.value;
-            if (selectedPage) {
-                window.location.href = selectedPage;
-            }
-        });
